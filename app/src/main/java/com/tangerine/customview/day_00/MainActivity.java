b38a64b0a144b69fe984340a0d5e3496074994c4
tangerine <947624811@qq.com>
2020-06-25 09:32:33 +0800

    add day_03

--- app/src/main/java/com/tangerine/customview/day_00/MainActivity.java
+++ app/src/main/java/com/tangerine/customview/day_00/MainActivity.java
@@ -1,66 +1,55 @@
 package com.tangerine.customview.day_00;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.tangerine.customview.R;
-import com.tangerine.customview.day_01.ColorTrackTextView;
+import com.tangerine.customview.day_02.ShapeView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private ColorTrackTextView mColorTrackTextView;
+
+    private Button button;
+    private ShapeView shapeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mColorTrackTextView = findViewById(R.id.colortrackTextView);
-        AppCompatButton rightToLeft = findViewById(R.id.right_to_left);
-        AppCompatButton leftToRight = findViewById(R.id.left_to_right);
-        rightToLeft.setOnClickListener(this);
-        leftToRight.setOnClickListener(this);
+        shapeView = findViewById(R.id.shapeView);
+        button = findViewById(R.id.exchange);
+        exchange();
     }
 
-    private void rightToLeft(){
-        mColorTrackTextView.setDirection(ColorTrackTextView.Direction.RIGHT_TO_LEFT);
-        ValueAnimator valueAnimator  = ObjectAnimator.ofFloat(0,1);
-        valueAnimator.setDuration(2000);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float currentProcess = (float) animation.getAnimatedValue();
-                mColorTrackTextView.setCurrentProcess(currentProcess);
-            }
-        });
-        valueAnimator.start();
+    private void exchange() {
+         new Thread(
+                 new Runnable() {
+                     @Override
+                     public void run() {
+                         while (true){
+                             shapeView.exchange();
+                             try {
+                                 Thread.sleep(1000);
+                             } catch (InterruptedException e) {
+                                 e.printStackTrace();
+                             }
+                         }
+                     }
+                 }
+         ).start();
 
-    }
-    private void leftToRight(){
-        mColorTrackTextView.setDirection(ColorTrackTextView.Direction.LEFT_TO_RIGHT);
-        ValueAnimator valueAnimator  = ObjectAnimator.ofFloat(0,1);
-        valueAnimator.setDuration(2000);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float currentProcess = (float) animation.getAnimatedValue();
-                mColorTrackTextView.setCurrentProcess(currentProcess);
-            }
-        });
-        valueAnimator.start();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.right_to_left:
-                rightToLeft();
-                break;
-            case R.id.left_to_right:
-                leftToRight();
+        switch (v.getId()) {
+            case R.id.exchange:
+
+
                 break;
             default:
                 break;
--- app/src/main/java/com/tangerine/customview/day_02/ShapeView.java
+++ app/src/main/java/com/tangerine/customview/day_02/ShapeView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,13 +13,14 @@ import androidx.annotation.Nullable;
 public class ShapeView extends View {
     private Shape mCurrentShape = Shape.CirCly;
     private Paint mPaint;
+    private Path mPath;
 
     public ShapeView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public ShapeView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public ShapeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -31,6 +33,11 @@ public class ShapeView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         //保证他是正方形
+
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(Math.min(width, height), Math.min(width, height));
+
     }
 
     @Override
@@ -38,22 +45,52 @@ public class ShapeView extends View {
         switch (mCurrentShape) {
             case CirCly:
                 //CirCly
-                int center = getWidth()/2;
+                int center = getWidth() / 2;
                 mPaint.setColor(Color.YELLOW);
-                canvas.drawCircle(center,center,center,mPaint);
+                canvas.drawCircle(center, center, center, mPaint);
                 break;
             case Square:
                 //Square
                 mPaint.setColor(Color.BLUE);
-                canvas.drawRect(0,0,getWidth(),getHeight(),mPaint);
+                canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
                 break;
             case Triangle:
+                //等边三角形
+                mPaint.setColor(Color.RED);
+                if (mPath == null) {
+                    mPath = new Path();
+                    mPath.moveTo((float)(getWidth() / 2) , 0);
+                    mPath.lineTo(0, (float) ((getWidth()/2 ) *Math.sqrt(3)));
+                    mPath.lineTo(getWidth(), (float) ((getWidth()/2) * Math.sqrt(3)));
+                    mPath.close();
+                }
+                //getWith /2 ,
+
+                canvas.drawPath(mPath,mPaint);
                 //Triangle
                 break;
             default:
                 break;
         }
 
+    }
+
+    public void exchange() {
+        switch (mCurrentShape) {
+            case CirCly:
+                mCurrentShape = Shape.Square;
+                break;
+            case Square:
+                mCurrentShape = Shape.Triangle;
+                break;
+            case Triangle:
+                mCurrentShape = Shape.CirCly;
+                break;
+            default:
+                break;
+
+        }
+        postInvalidate();
     }
 
     public enum Shape {
