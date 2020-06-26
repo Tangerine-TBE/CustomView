d72c893f5ff9c936bb4d46e39b6e36114c0ca632
Tangerrine <1762094255@qq.com>
2020-06-26 11:38:46 +0800

    完善LetterSideBar效果实现，等待后期整合RecyclerView进行滚动条目测试

--- app/src/main/java/com/tangerine/customview/day_04/LetterSideBar.java
+++ app/src/main/java/com/tangerine/customview/day_04/LetterSideBar.java
@@ -20,9 +20,11 @@ public class LetterSideBar extends View {
     private int mTextSize = 40;
     private int mOriginTextColor = Color.BLUE;
     private int mChangeTextColor = Color.YELLOW;
-    private Paint mTextPaint;
+    private Paint mSideBarOriginTextPaint;
+    private Paint mSideBarChangeTextPaint;
 
-    private String [] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","#"};
+    private String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#"};
+    private String mCurrentTouchLetter; //当前触摸位置的字母
 
     public LetterSideBar(Context context) {
         this(context, null);
@@ -39,13 +41,22 @@ public class LetterSideBar extends View {
 
     private void init(Context context, AttributeSet attrs) {
 
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.LetterSideBar);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LetterSideBar);
         mOriginTextColor = typedArray.getColor(R.styleable.LetterSideBar_textOriginColor, mOriginTextColor);
         mChangeTextColor = typedArray.getColor(R.styleable.LetterSideBar_textChangeColor, mChangeTextColor);
         mTextSize = typedArray.getDimensionPixelSize(R.styleable.LetterSideBar_textSize, mTextSize);
-        mTextPaint = new Paint();
-        mTextPaint.setAntiAlias(true);
-        mTextPaint.setTextSize(mTextSize);
+        mSideBarOriginTextPaint = new Paint();
+        mSideBarOriginTextPaint.setAntiAlias(true);
+        mSideBarOriginTextPaint.setTextSize(mTextSize);
+        mSideBarOriginTextPaint.setColor(mOriginTextColor);
+
+
+        mSideBarChangeTextPaint = new Paint();
+        mSideBarChangeTextPaint.setAntiAlias(true);
+        mSideBarChangeTextPaint.setTextSize(mTextSize);
+        mSideBarChangeTextPaint.setColor(mChangeTextColor);
+
+
 
         typedArray.recycle();
 
@@ -56,7 +67,7 @@ public class LetterSideBar extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         //计算指定宽度 =  padding + 字体宽度
-        int textWidth = (int) mTextPaint.measureText("W");
+        int textWidth = (int) mSideBarOriginTextPaint.measureText("W");
         int width = getPaddingLeft() + getPaddingRight() + textWidth;
         int height = MeasureSpec.getSize(heightMeasureSpec);
         setMeasuredDimension(width, height);
@@ -67,6 +78,31 @@ public class LetterSideBar extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
             case MotionEvent.ACTION_DOWN:
+                float currentMoveY = event.getY();
+                //当前的高度/字母的高度   通过位置获取字母
+                int itemHeight = (getHeight() - getPaddingTop() - getPaddingBottom()) / letters.length; //拿到每一个字母应该有的高度
+                int currentPosition = (int) (currentMoveY /itemHeight ) ; //拿到每一个字母应该有的高度
+                if (currentPosition < 0){
+                    currentPosition = 0;
+                }
+                if (currentPosition > letters.length - 1){
+                    currentPosition = letters.length -1;
+                }
+                if (!letters[currentPosition].equals(mCurrentTouchLetter)){
+                    mCurrentTouchLetter = letters[currentPosition];
+
+                    if (mLetterTouchListener != null){
+                        mLetterTouchListener.touch(mCurrentTouchLetter);
+                    }
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (mLetterTouchListener != null){
+                    mLetterTouchListener.up();
+                }
+                mCurrentTouchLetter="";
+                invalidate();
                 break;
             default:
                 break;
@@ -77,15 +113,30 @@ public class LetterSideBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         //x 绘制在最中间 = getWidth /2 - 文字/2
-        int itemHeight = (getHeight() - getPaddingTop() -getPaddingBottom())/ letters.length; //拿到每一个字母应该有的高度
-        for (int i =0 ; i < letters.length ; i ++){
-            int itemCenterY = i *  itemHeight + itemHeight /2 + getPaddingTop();
-            Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-            int dy = (int) ((fontMetrics.bottom - fontMetrics.top)/2 - fontMetrics.bottom);
+        int itemHeight = (getHeight() - getPaddingTop() - getPaddingBottom()) / letters.length; //拿到每一个字母应该有的高度
+        for (int i = 0; i < letters.length; i++) {
+            int itemCenterY = i * itemHeight + itemHeight / 2 + getPaddingTop();
+            Paint.FontMetrics fontMetrics = mSideBarOriginTextPaint.getFontMetrics();
+            int dy = (int) ((fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom);
             int baseLineY = itemCenterY + dy;
-            int textWidth = (int) mTextPaint.measureText(letters[i]);
-            int x  = getWidth()/2  -textWidth /2 ;
-            canvas.drawText(letters[i],x,baseLineY,mTextPaint);
+            int textWidth = (int) mSideBarOriginTextPaint.measureText(letters[i]);
+            int x = getWidth() / 2 - textWidth / 2;
+
+            //当前字母高亮
+            if(letters[i].equals(mCurrentTouchLetter)){
+                canvas.drawText(letters[i],x ,baseLineY,mSideBarChangeTextPaint);
+            }else{
+                canvas.drawText(letters[i],x,baseLineY,mSideBarOriginTextPaint);
+            }
         }
+    }
+    private LetterTouchListener mLetterTouchListener;
+    public void setOnLetterTouchListener(LetterTouchListener letterTouchListener){
+        this.mLetterTouchListener = letterTouchListener;
+    }
+
+    public  interface  LetterTouchListener{
+        void touch(String currentLetter);
+        void up();
     }
 }
--- app/src/main/java/com/tangerine/customview/day_04/LetterSideBarActivity.java
+++ app/src/main/java/com/tangerine/customview/day_04/LetterSideBarActivity.java
@@ -1,16 +1,33 @@
 package com.tangerine.customview.day_04;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.tangerine.customview.R;
 
-public class LetterSideBarActivity extends AppCompatActivity {
-
+public class LetterSideBarActivity extends AppCompatActivity implements LetterSideBar.LetterTouchListener {
+    private AppCompatTextView mTvLetter;
+    private LetterSideBar mLetterSideBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letter_side_bar);
+        mTvLetter = findViewById(R.id.tv_letter);
+        mLetterSideBar = findViewById(R.id.letterSideBar);
+        mLetterSideBar.setOnLetterTouchListener(this);
+    }
+
+    @Override
+    public void touch(String currentLetter) {
+        mTvLetter.setText(currentLetter);
+        mTvLetter.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void up() {
+        mTvLetter.setVisibility(View.GONE);
     }
 }
